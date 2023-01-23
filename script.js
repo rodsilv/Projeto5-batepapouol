75ac49907bdcd9f315f87929b48017f088c23091
Rodrigo Silva <chggrod@gmail.com>
2023-01-23 17:06:54 -0300

    finalizando

--- script.js
+++ script.js
@@ -7,58 +7,78 @@ let localmsg = [{
     time: "08:01:17"
 }];
 
+
 function logarnasala(){
     do { usuario = prompt("Digite o seu nome");}
     while (usuario === "");
     const nome = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", {name: usuario});
-    nome.then(loguin);
 }
 
-logarnasala();
 
 function ficarlogado(){
-axios.post("https://mock-api.driven.com.br/api/v6/uol/status", {name: usuario});
+axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: usuario });
 }
 
-setInterval(ficarlogado, 5000);
-
-pegarmsgnoservidor();
 
 function pegarmsgnoservidor(){
-   const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-   promessa.then(msgchegaram);
-   promessa.catch(erropegarmsg);
-}
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promessa.then(msgchegaram);
+    promessa.catch(erropegarmsg);
+ }
+ 
 
 function msgchegaram(resposta){
-    console.log('msg chegou!!');
-    console.log(resposta.data);
-    localmsg = resposta.data;
-    mostrarmsg(resposta.data);
-    
+    document.querySelector(".local-feed").innerHTML = '';
+     console.log('msg chegou!!');
+     console.log(resposta.data);
+     localmsg = resposta.data
+     localmsg.map((k)=>{
+        if (k.type==="status"){
+            let inorout = k.from
+            let texto = k.text
+            let hora = k.time
+            msgstatus(inorout,texto,hora)}
+        else if(k.type==="message"){
+                let inorout =k.from
+                let hora = k.time
+                let texto = k.text
+                let quem = k.to
+                msgdopovo(inorout,hora,texto,quem)}
+                else if(k.type==="private"){
+                    let inorout =k.from
+                    let hora = k.time   
+                    let texto = k.text
+                    let quem = k.to
+                    msgpvt(inorout,hora,texto,quem)}
+})
+msgfinal();
 }
+
+
+
+function msgstatus(inorout,texto,hora){
+    document.querySelector(".local-feed").innerHTML +=`
+    <div data-test="message" class="msgserve"> <div class="hora"> (${hora}) </div> <div class="inorout"> ${inorout} </div> <div class="status"> ${texto} </div></div>
+    `}
+
+
+function msgdopovo(inorout,hora,texto,quem){
+        document.querySelector(".local-feed").innerHTML +=`
+        <div data-test="message" class="msgserve povo"> <div class="hora"> (${hora}) </div> <div class="inorout"> ${inorout}  </div> <p class="status2"> para</p> <div class="inorout"> ${quem}: </div> <div class="status"> ${texto} </div></div>
+        `}
+
+
+function msgpvt(inorout,hora,texto,quem){
+    document.querySelector(".local-feed").innerHTML +=`
+    <div data-test="message" class="msgserve  pvt"> <div class="hora"> (${hora}) </div> <div class="inorout"> ${inorout}  </div> <p class="status2"> para</p> <div class="inorout"> ${quem}: </div> <div class="status"> ${texto} </div></div>
+    `}
+
 
 function erropegarmsg(erro){
     console.log('erro ao pegar msg');
     console.log(erro);
-    errototal();
+    relogar();
 }
-
-
-function mostrarmsg(){
-    const msgtotal = document.querySelector(".local-feed");
-msgtotal.innerHTML = '';
-for( let k = 0; k < localmsg.length; k++){
-    let tamplate = `
-    <li>
-        ${localmsg[k].time}
-    </li>
-    `;
-    msgtotal.innerHTML = msgtotal.innerHTML + tamplete;
-}}
-
-mostrarmsg();
-
 
 
 function enviarmsg(){
@@ -72,3 +92,19 @@ function enviarmsg(){
     axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", objmsg).then(pegarmsgnoservidor)
     document.querySelector(".escreva-aqui input").value = "" 
 }
+
+
+
+function msgfinal(){
+const elementoQueQueroQueApareca = document.querySelector('.msgserve');
+elementoQueQueroQueApareca.scrollIntoView();
+}
+
+function relogar(){
+    window.location.reload();
+}
+
+logarnasala();
+pegarmsgnoservidor();
+setInterval(ficarlogado, 5000);
+setInterval(pegarmsgnoservidor, 3000);
